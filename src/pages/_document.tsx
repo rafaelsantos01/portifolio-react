2908fcbc69cd22975b6bc4a0978efb7040edc279
Rafael P. Santos <rafinhapsantos50@gmail.com>
2023-11-02 22:19:24 -0300

    ajuste

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <title>Rafael Santos</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/src/assets/logo.png" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Rafael Santos" />
+      </Head>
       <body>
         <Main />
         <NextScript />
