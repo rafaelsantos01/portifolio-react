1bfaccef02914bf6d7e03cecfad11ad5ecdf4a0f
Rafael Santos <rafinhapsantos50@gmail.com>
2024-01-27 12:03:18 -0300

    :lipstick: feat: adição efeito maquina de escrever

--- src/components/pages/about/about.tsx
+++ src/components/pages/about/about.tsx
@@ -3,7 +3,6 @@ import myPhoto from "@/assets/P_20191226_185840.jpg";
 import Container from "@/components/container/container";
 import Title from "@/components/title/title";
 
-
 export default function About() {
   return (
     <Container>
@@ -25,23 +24,26 @@ export default function About() {
           <div className="select-none">
             <p className="mb-2">
               Meu nome é Rafael, um desenvolvedor Full-Stack com mais de um ano
-               de experiência, atualmente residindo em Brusque, SC.
+              de experiência, atualmente residindo em Brusque, SC.
             </p>
             <p className="mb-2">
-              Iniciei minha carreira como técnico de informática, acumulando mais de sete anos de experiência em manutenção de computadores, 
-            servidores e redes, colaborando com empresas de destaque na região. Essa sólida base preparou o terreno para minha transição para a programação, onde 
-            encontrei minha verdadeira paixão.
-            Como desenvolvedor Full-Stack, faço parte da equipe da Quantus há mais de um ano. Nessa jornada, contribuí para projetos desafiadores,
-            incluindo a implementação de tecnologias como Kafka e Keycloak, além do desenvolvimento de uma arquitetura de microsserviços.
+              Iniciei minha carreira como técnico de informática, acumulando
+              mais de sete anos de experiência em manutenção de computadores,
+              servidores e redes, colaborando com empresas de destaque na
+              região. Essa sólida base preparou o terreno para minha transição
+              para a programação, onde encontrei minha verdadeira paixão. Como
+              desenvolvedor Full-Stack, faço parte da equipe da Quantus aonde
+              estou há mais de um ano. Nessa jornada, contribuí para projetos
+              desafiadores, dentre esses desafios destaco a implementação de
+              tecnologias como Kafka,Keycloak autenticação jwt dentre outros.
             </p>
             <p className="mb-2">
-              Minha abordagem à programação é impulsionada pela curiosidade e pela vontade de enfrentar desafios complexos. Ao longo do tempo, 
-            desenvolvi uma série de projetos pessoais, cada um adicionando um novo nível de complexidade à minha habilidade como desenvolvedor. Atualmente, 
-            busco a evolução constante e a transição para a posição de desenvolvedor Pleno.
-            
-            </p>
-            <p className="mb-2">
-            Estou ansioso para o que o futuro reserva em minha carreira na tecnologia e estou sempre em busca de novas oportunidades de crescimento e aprendizado.
+              Minha abordagem à programação é impulsionada pela curiosidade e
+              pela vontade de enfrentar desafios complexos. Ao longo do tempo,
+              desenvolvi uma série de projetos pessoais, cada um adicionando um
+              novo nível de complexidade à minha habilidade como desenvolvedor.
+              Atualmente, busco a evolução constante e a transição para a
+              posição de desenvolvedor Pleno.
             </p>
           </div>
         </div>
